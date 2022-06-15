1df41f5d90a3fb0c238e33c440613215ae8b86b3
LiMi-SEDC <limimisini@hotmail.com>
2022-06-15 16:45:24 +0200

    navbar ...

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { Box } from '@mui/material';
+
+import { Home, ExerciseDetail } from './pages';
+import { Footer, Navbar } from './components';
+
 import './App.css';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercise/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
 };
 
 export default App;
